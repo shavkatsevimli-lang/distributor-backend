1bf068bb877a8823075671c6a4a3fb17c9f92d39
shavkatsevimli-lang <shavkatsevimli@gmail.com>
2026-04-19 23:34:45 +0500

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,12 @@
 import { Body, Controller, Get, Post, UnauthorizedException } from '@nestjs/common';
 
+let orders: any[] = [];
+
 @Controller()
 export class AppController {
   @Get()
   getHello(): string {
     return 'Hello';
-  }
-
-  @Get('users')
-  getUsers() {
-    return [
-      { id: 1, name: 'Ali' },
-      { id: 2, name: 'Vali' }
-    ];
   }
 
   @Get('products')
@@ -25,9 +19,32 @@ export class AppController {
     ];
   }
 
+  @Get('orders')
+  getOrders() {
+    return orders;
+  }
+
+  @Post('orders')
+  createOrder(@Body() body: any) {
+    const newOrder = {
+      id: orders.length + 1,
+      ...body,
+      status: 'new',
+      createdAt: new Date().toISOString(),
+    };
+
+    orders.push(newOrder);
+
+    return {
+      success: true,
+      message: 'Zakaz qabul qilindi',
+      order: newOrder,
+    };
+  }
+
   @Post('login')
   login(@Body() body: { phone: string; password: string }) {
-    const demoUser = {
+    const demoClient = {
       id: 1,
       fullName: 'Ali Market',
       phone: '998901234567',
@@ -35,15 +52,36 @@ export class AppController {
       role: 'client',
     };
 
-    if (body.phone === demoUser.phone && body.password === demoUser.password) {
+    const demoAdmin = {
+      id: 2,
+      fullName: 'Admin',
+      phone: '999',
+      password: '999',
+      role: 'admin',
+    };
+
+    if (body.phone === demoClient.phone && body.password === demoClient.password) {
       return {
         success: true,
         message: 'Login successful',
         user: {
-          id: demoUser.id,
-          fullName: demoUser.fullName,
-          phone: demoUser.phone,
-          role: demoUser.role,
+          id: demoClient.id,
+          fullName: demoClient.fullName,
+          phone: demoClient.phone,
+          role: demoClient.role,
+        },
+      };
+    }
+
+    if (body.phone === demoAdmin.phone && body.password === demoAdmin.password) {
+      return {
+        success: true,
+        message: 'Login successful',
+        user: {
+          id: demoAdmin.id,
+          fullName: demoAdmin.fullName,
+          phone: demoAdmin.phone,
+          role: demoAdmin.role,
         },
       };
     }
